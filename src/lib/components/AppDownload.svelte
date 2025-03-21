010084f3d7ff2a21796d90939b2dab0fd8f5f183
Tero Saarikivi <saarikivit@gmail.com>
2025-03-21 16:02:12 +0200

    chore: add daisy

--- src/lib/components/AppDownload.svelte
+++ src/lib/components/AppDownload.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 </script>
 
-<div class="flex items-center justify-center gap-4 text-[#7A7777]">
-	<p class="text-lg">Download Sonofield Ear Trainer for iOS and Android.</p>
+<div class="flex items-center justify-center gap-4">
+	<p class="text-accent text-lg">Download Sonofield Ear Trainer for iOS and Android.</p>
 	<a
 		href="https://et.sonofield.com"
 		aria-label="Download Sonofield Ear Trainer"
-		class="rounded-lg border border-[#7A7777] bg-[#1A1A1D] px-4 py-2 transition-colors hover:bg-[#7A7777] hover:text-[#1A1A1D]"
+		class="btn btn-outline btn-accent"
 	>
 		DOWNLOAD
 	</a>
